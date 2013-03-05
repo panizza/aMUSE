6508a231a63b999c3255cb3d7de7ea8131247b0e
Andrea Panizza <panizza01@gmail.com>
2013-03-05 20:15:57 +0100

    add __unicode__ to models

--- aMuse/api/handlers.py
+++ aMuse/api/handlers.py
@@ -4,8 +4,10 @@ from basetyzer.models import Item
 class ItemHandler(BaseHandler):
     allowed_methods = ('GET',)
     model = Item
-    #fields = (
-    exclude = ('_state')
+    fields = ('description', 'author', 'photo', 'release_date', 'title', 'id',
+              ('nfc_tag', ('serial', 'id', 'in_use'))
+    )
+    #exclude = ('_state')
     def read(self, request, item_id):
         return Item.objects.get(id=item_id)
         
--- aMuse/basetyzer/models.py
+++ aMuse/basetyzer/models.py
@@ -7,10 +7,16 @@ class Exhibit(models.Model):
     date_begin = models.DateField()
     date_end = models.DateField()
     owner = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "%s" % (self.name,)
     
 class NFC(models.Model):
     serial = models.CharField(max_length=50)
     in_use = models.BooleanField()
+
+    def __unicode__(self):
+        return "%s" % (self.serial,)
 
 class Item(models.Model):
     title = models.CharField(max_length=50)
@@ -20,28 +26,51 @@ class Item(models.Model):
     photo = models.ImageField(upload_to="images/items/")
     nfc_tag = models.ForeignKey(NFC)
     exhibit = models.ManyToManyField(Exhibit)
+
+    def __unicode__(self):
+        return "%s" % (self.title,)
     
 class Experience(models.Model):
     date = models.DateTimeField()
     user = models.ForeignKey(User)
     confirmed = models.BooleanField()
     hash_url = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return "%s - %s" % (self.user.username, self.date,)
     
     def hashit(self):
         from hashlib import sha1
         return sha1(self.pk).hexdigest()
 
+
 class Comment(models.Model):
     content = models.TextField()
-    
+
+    def __unicode__(self):
+        return "%s" % (self.content,)
+
+
 class Photo(models.Model):
     content = models.ImageField(upload_to="images/userphoto/")
-    
+
+    def __unicode__(self):
+        return "%s" % (self.content.name,)
+
+
 class Scan(models.Model):
     content = models.ForeignKey(Item)
-    
+
+    def __unicode__(self):
+        return "%s" % (self.content,)
+
+
 class Action(models.Model):
     date_performed = models.DateTimeField()
     scan = models.ForeignKey(Scan)
     photo = models.ForeignKey(Photo)
     comment = models.ForeignKey(Comment)
+    experience = models.ForeignKey(Experience)
+
+    def __unicode__(self):
+        return "%s" % (self.experience, self.date)
