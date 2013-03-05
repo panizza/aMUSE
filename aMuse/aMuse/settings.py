1f04231d9ea1dc8f1f9dd9c391aa38052d387fad
Andrea Panizza <panizza01@gmail.com>
2013-03-05 22:16:34 +0100

    added shell_plus

--- aMuse/aMuse/settings.py
+++ aMuse/aMuse/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
+    'django_extensions',
     'api',
     'basetyzer',
 )
