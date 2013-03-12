e27ae3a10c7e1c2b83137d72cd306ceb29439387
Mirko <nanotrippolo@gmail.com>
2013-03-12 08:48:39 +0100

    sparkle
    now login page sparkles

--- aMuse/webinator/urls.py
+++ aMuse/webinator/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib import admin
 from django.contrib.auth.views import login, logout
-admin.autodiscover()
 
 urlpatterns = patterns('webinator.views',
         url(r'^login/', login, name='login'),
