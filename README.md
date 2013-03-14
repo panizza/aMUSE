94b26479ef43f4832c7927d4ab3f12e039831d11
Andrea Panizza <panizza01@gmail.com>
2013-03-14 13:52:00 +0100

    pimp my fix

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 aMUSEment for the masses
 =====
-
-
-
-
-
--- aMuse/api/urls.py
+++ aMuse/api/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
-from api.views import get_item, save_visit
 
 
 urlpatterns = patterns('api.views',
     url(r'^info/(?P<id_item>\d+)/$', 'get_item', name="get_info"),
-    url(r'^save/$', 'save_visit', name="save_visit"),
+    url(r'^save/$', 'visit_save', name="save_visit")
 )
