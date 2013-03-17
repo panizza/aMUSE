eebc559fc46f2810331b7430794cb71708381dfd
Mirko <nanotrippolo@gmail.com>
2013-03-17 14:39:00 +0100

    fixed login and csrf problem
    It's a "working solution",don't know if the best one

--- aMuse/webinator/urls.py
+++ aMuse/webinator/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login, logout
+
 import webinator.views
 
 urlpatterns = patterns('webinator.views',
-        url(r'^login/', login, name='login'),
+        url(r'^login/(?P<error>\w+)','show_login_error',name='login-fail'),
+        url(r'^login',login,name='login'),
         url(r'^logout/', logout, name='logout'),
         url(r'^logging/','do_login'),
 )
--- aMuse/webinator/views.py
+++ aMuse/webinator/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.core.context_processors import csrf
+
 import webinator.urls
 
 def do_login(request):
@@ -13,5 +18,15 @@ def do_login(request):
         if user is not  None:
             return render_to_response('webinator/user_details.html',{'user':user})
         else:
-            return render_to_response('registration/login.html',{'state':'Wrong username or password'})
+            url = reverse('login-fail',kwargs={'error':'login_fail'})
+            return  HttpResponseRedirect(url)
+            #return render_to_response('registration/login.html',{'state':'Wrong username or password'})
 
+def show_login_error(request,error):
+    if error == 'login_fail':
+        return render_to_response('registration/login.html',{'state':'Wrong username or password'},
+            context_instance =RequestContext(request))
+    #This could be useful in future...maybe
+    if error == 'logout':
+        return render_to_response('registration/login.html',{'state':'You have been logout correctly'},
+            context_instance =RequestContext(request))
