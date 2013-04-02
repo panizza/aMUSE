24cdb2ce935eb490bf214b3258c258ecbdb7b5fb
Andrea Panizza <panizza01@gmail.com>
2013-04-02 15:49:12 +0200

    now images url works

--- aMuse/aMuse/settings.py
+++ aMuse/aMuse/settings.py
@@ -32,27 +32,13 @@ DATABASES = {
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Rome'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
@@ -62,21 +48,20 @@ MEDIA_ROOT = 'media'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = ''
-STATIC_ROOT = join(SETTINGS_PATH, 'static_all')
+STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    join(SETTINGS_PATH, '../webinator/static'),
+    #join(SETTINGS_PATH, '../media'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -97,7 +82,6 @@ SECRET_KEY = '+3j5d@$8flg05_x&3lrzo0(-4wx4hsep#+epp7^%^mvnwr+ar1'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -106,8 +90,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'aMuse.urls'
@@ -118,9 +100,8 @@ WSGI_APPLICATION = 'aMuse.wsgi.application'
 
 # Find templates in the same folder as settings.py.
 TEMPLATE_DIRS = (
-   join(SETTINGS_PATH, '../webinator/templates')
    #"/Users/les/sites/env/amuse/project/aMUSE/aMuse/webinator/templates/webinator"
-    )
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
--- aMuse/aMuse/urls.py
+++ aMuse/aMuse/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 
@@ -15,3 +16,6 @@ urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
