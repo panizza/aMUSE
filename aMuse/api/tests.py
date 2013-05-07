eabf9a49b2db74bd8d648e931ef9db1f3dad0dee
Lorenzo Lotto <llorenzo@outlook.com>
2013-05-07 08:34:45 +0200

    removed useless
    removed wrong tests

--- aMuse/api/tests.py
+++ aMuse/api/tests.py
@@ -3,40 +3,11 @@ from basetyzer.models import Exhibit
 from django.core.urlresolvers import reverse
 import json as j
 
-
-class ExhibitionTest(TestCase):
-    fixtures = ['percussion.json', 'with_experience.json']
-
-    def setUp(self):
-        self.client = Client()
-
-    def test_exhibitions_list(self):
-        response = self.client.get('/api/e/')
-        self.assertEqual(response.status_code, 200)
-        json = j.loads(response.content)
-        self.assertEqual(len(json['data']), 2)
-
-    def test_exhibition_info(self):
-        response = self.client.get('/api/e/54326435842647/')
-        self.assertEqual(response.status_code, 404)
-        response = self.client.get('/api/e/1/')
-        self.assertEqual(response.status_code, 200)
-
-
 class ItemTest(TestCase):
-    fixtures = ['percussion_new_db.json', 'all_data.json']
+    fixtures = ['all_data.json']
 
     def setUp(self):
         self.client = Client()
-
-    def test_item_list(self):
-        response = self.client.get('/api/e/43265435756867/i/')
-        self.assertEqual(response.status_code, 404)
-        response = self.client.get('/api/e/1/i/')
-        self.assertEqual(response.status_code, 200)
-        json = j.loads(response.content)
-        self.assertTrue(json.get('data'))
-
 
     def test_get_item_info(self):
         response = self.client.get('/api/i/43265435756867/')
